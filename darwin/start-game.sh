6da756a4e2730ae82144e5cf1278d299fe32cdfd
mgracer48 <mgracer48@users.noreply.github.com>
2015-10-24 12:16:00 +0200

    Update start-game.sh

--- darwin/start-game.sh
+++ darwin/start-game.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 cd ..
 
-export DYLD_LIBRARY_PATH=`pwd`/Libraries.bundle
-export DYLD_FRAMEWORK_PATH="Frameworks"
-
 # Get the user input:
 read -p "Username: " ttiUsername
 read -p "Gameserver (DEFAULT:  167.114.28.238): " TTI_GAMESERVER
@@ -11,7 +8,6 @@ TTI_GAMESERVER=${TTI_GAMESERVER:-"167.114.28.238"}
 
 # Export the environment variables:
 export ttiUsername=$ttiUsername
-export ttiPassword="password"
 export TTI_PLAYCOOKIE=$ttiUsername
 export TTI_GAMESERVER=$TTI_GAMESERVER
 
