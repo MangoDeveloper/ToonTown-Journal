9dc545c7978534923a97ccc7a20345afecff1460
Michael <mgracer48@yahoo.com>
2015-10-26 15:53:20 +0100

    fix parties :)

--- toontown/parties/PartyGlobals.py
+++ toontown/parties/PartyGlobals.py
@@ -10,7 +10,7 @@ MaxPlannedYear = 2030
 MinPlannedYear = 1975
 JellybeanMultiplier = 1.5
 JellyBeanDayMultiplier = 2
-PARTY_DURATION = 1800
+PARTY_DURATION = 1800.0
 EventsPageGuestNameMaxWidth = 0.42
 EventsPageGuestNameMaxLetters = 18
 EventsPageHostNameMaxWidth = 0.37
@@ -30,7 +30,7 @@ JarLabelMaxedTextColor = (1.0,
 TuftsOfGrass = 75
 MaxToonsAtAParty = 20
 DefaultPartyDuration = 0.5
-DelayBeforeAutoKick = 1.0
+DelayBeforeAutoKick = 30.0
 MaxHostedPartiesPerToon = 1
 PartyEditorGridBounds = ((-0.11, 0.289), (0.55, -0.447))
 PartyEditorGridCenter = (PartyEditorGridBounds[0][0] + (PartyEditorGridBounds[1][0] - PartyEditorGridBounds[0][0]) / 2.0, PartyEditorGridBounds[1][1] + (PartyEditorGridBounds[0][1] - PartyEditorGridBounds[1][1]) / 2.0)
@@ -90,7 +90,7 @@ ActivityIds = PythonUtil.Enum(('PartyJukebox',
  'PartyValentineJukebox',
  'PartyValentineJukebox40',
  'PartyValentineTrampoline'))
-PartyEditorActivityOrder = [ ActivityIds.PartyClock,
+PartyEditorActivityOrder = [ActivityIds.PartyClock,
  ActivityIds.PartyJukebox,
  ActivityIds.PartyJukebox40,
  ActivityIds.PartyValentineJukebox,
@@ -164,7 +164,7 @@ DecorationIds = PythonUtil.Enum(('BalloonAnvil',
  'snowman',
  'snowDoodle',
  'BalloonAnvilValentine'))
-TTIUnreleasedDecor = [DecorationIds.HeartTarget,
+TTOUnreleasedDecor = [DecorationIds.HeartTarget,
  DecorationIds.HeartBanner,
  DecorationIds.FlyingHeart,
  DecorationIds.Hydra,
@@ -231,7 +231,7 @@ PlayGroundToPartyClockColors = {'the_burrrgh': (53.0 / 255.0,
 PartyGridUnitLength = [14.4, 14.6]
 PartyGridHeadingConverter = 15.0
 PartyGridToPandaOffset = (-PartyGridUnitLength[0] * PartyEditorGridSize[0] / 2.0, -PartyGridUnitLength[1] * PartyEditorGridSize[1] / 2.0)
-PartyCostMultiplier = 0 # ALPHA ONLY - remove after parties are legit
+PartyCostMultiplier = 1
 MinimumPartyCost = 100 * PartyCostMultiplier
 ActivityInformationDict = {ActivityIds.PartyJukebox: {'cost': int(50 * PartyCostMultiplier),
                             'gridsize': (1, 1),
@@ -667,7 +667,7 @@ for type in DropObjectTypes:
 Name2DOTypeId = {}
 names = Name2DropObjectType.keys()
 names.sort()
-for i in xrange(len(names)):
+for i in range(len(names)):
     Name2DOTypeId[names[i]] = i
 
 DOTypeId2Name = names
@@ -762,7 +762,7 @@ MUSIC_MIN_LENGTH_SECONDS = 50.0
 MUSIC_GAP = 2.5
 PhaseToMusicData = {3.5: {'TC_SZ.ogg': [TTLocalizer.MusicTcSz, 57]},
  3: {'create_a_toon.ogg': [TTLocalizer.MusicCreateAToon, 175],
-     'tt_theme.ogg': [TTLocalizer.MusicTtTheme, 51]},
+     'tt_theme.mp3': [TTLocalizer.MusicTtTheme, 51]},
  4: {'TC_nbrhood.ogg': [TTLocalizer.MusicTcNbrhood, 59],
      'MG_TwoDGame.ogg': [TTLocalizer.MusicMgTwodgame, 60],
      'MG_Vine.ogg': [TTLocalizer.MusicMgVine, 32],
@@ -783,7 +783,7 @@ PhaseToMusicData = {3.5: {'TC_SZ.ogg': [TTLocalizer.MusicTcSz, 57]},
 PhaseToMusicData40 = {3.5: {'encntr_general_bg.ogg': [TTLocalizer.MusicEncntrGeneralBg, 30],
        'TC_SZ.ogg': [TTLocalizer.MusicTcSz, 57]},
  3: {'create_a_toon.ogg': [TTLocalizer.MusicCreateAToon, 175],
-     'tt_theme.ogg': [TTLocalizer.MusicTtiTheme, 51]},
+     'tt_theme.mp3': [TTLocalizer.MusicTtTheme, 51]},
  4: {'minigame_race.ogg': [TTLocalizer.MusicMinigameRace, 77],
      'TC_nbrhood.ogg': [TTLocalizer.MusicTcNbrhood, 59],
      'MG_TwoDGame.ogg': [TTLocalizer.MusicMgTwodgame, 60],
