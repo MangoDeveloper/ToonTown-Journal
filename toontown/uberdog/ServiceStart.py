32d2fd4427cde27b75eb693de8e0d16dd980bb07
Wilee999 <Wilee999@users.noreply.github.com>
2015-10-28 12:51:42 -0700

    Update ServiceStart.py

--- toontown/uberdog/ServiceStart.py
+++ toontown/uberdog/ServiceStart.py
@@ -52,8 +52,6 @@ def openInjector_wx():
     frame.Show()
     app.SetTopWindow(frame)
     
-    textbox.AppendText(defaultText)
-    
     threading.Thread(target = app.MainLoop).start()
 
 openInjector_wx()
