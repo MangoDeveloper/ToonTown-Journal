87598ac34ae57964a150ca17d4f6510a130cf023
Michael <mgracer48@yahoo.com>
2015-10-29 18:21:47 +0100

    forgot to add silly saturday

--- toontown/toonbase/TTLocalizerEnglish.py
+++ toontown/toonbase/TTLocalizerEnglish.py
@@ -9928,3 +9928,6 @@ def convertSecondsToDate(seconds):
 
     return '%d:%02d:%02d' % (h, m, s)
 
+SillySaturdayStart = "It's time for Silly Saturday! Saturdays are silly with Fish Bingo and Grand Prix throughout the day!"
+SillySaturdayOngoing = 'Welcome! Silly Saturday is currently in progress.'
+SillySaturdayEnd = 'Silly Saturday is over. Hope you had fun. See you next week!'
