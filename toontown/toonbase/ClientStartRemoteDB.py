f550d0c86ece7e10c11117c1e7e904ce11e72f32
Wilee999 <Wilee999@users.noreply.github.com>
2015-10-28 17:36:07 -0700

    Update ClientStartRemoteDB.py

--- toontown/toonbase/ClientStartRemoteDB.py
+++ toontown/toonbase/ClientStartRemoteDB.py
@@ -2,37 +2,7 @@
 import json
 import os
 import requests
-from pandac.PandaModules import *
-
-#due to packing and compiling,
-#this module can only be accessed by devs,
-#so its safe to debug here.
-
-def runInjectorCode():
-        global text
-        exec (text.get(1.0, "end"),globals())
-
-def openInjector():
-    import Tkinter as tk
-    from direct.stdpy import thread
-    root = tk.Tk()
-    root.geometry('600x400')
-    root.title('Toontown Journey Remote Debug Injector')
-    root.resizable(False,False)
-    global text
-    frame = tk.Frame(root)
-    text = tk.Text(frame,width=70,height=20)
-    text.pack(side="left")
-    tk.Button(root,text="Inject!",command=runInjectorCode).pack()
-    scroll = tk.Scrollbar(frame)
-    scroll.pack(fill="y",side="right")
-    scroll.config(command=text.yview)
-    text.config(yscrollcommand=scroll.set)
-    frame.pack(fill="y")
-
-    thread.start_new_thread(root.mainloop,())
-
-openInjector()
+from panda3d.core import *
 
 username = os.environ['ttjUsername']
 password = os.environ['ttjPassword']
